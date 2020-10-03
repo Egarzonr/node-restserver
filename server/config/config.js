00c0de41c0ae157673245c71d3b6536f3030e3ef
ErnestoGarzon <ernestogarzon@subocol.com>
2020-10-02 22:21:01 -0500

    Variable entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://m220student:m220password@mflix.ktu1t.mongodb.net/cafe?authSource=admin&replicaSet=atlas-tgpalt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
